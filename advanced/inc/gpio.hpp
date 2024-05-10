58dfcbb11539e857b956befec43eede20218852e
naushada <naushad.dln@gmail.com>
2024-05-11 00:30:18 +0530

    Updated

--- advanced/inc/gpio.hpp
+++ advanced/inc/gpio.hpp
@@ -6,6 +6,54 @@
 
 class GPIO {
     public:
+        enum Register: std::uint32_t {
+            /* GPIO Function Selection offset */
+            BCM2837_GPFSEL0,
+            BCM2837_GPFSEL1,
+            BCM2837_GPFSEL2,
+            BCM2837_GPFSEL3,
+            BCM2837_GPFSEL4,
+            BCM2837_GPFSEL5,
+            BCM2837_GPFSEL_RESERVED,
+            /* GPIO SET Address offset */
+            BCM2837_GPSET0,
+            BCM2837_GPSET1,
+            BCM2837_GPSET_RESERVED,
+            /* Offset for clearing the GPIO */
+            BCM2837_GPCLR0,
+            BCM2837_GPCLR1,
+            BCM2837_GPCLR_RESERVED,
+            /* Offset for GPIO LEV0/1 */
+            BCM2837_GPLEV0,
+            BCM2837_GPLEV1,
+            BCM2837_GPLEV_RESERVED,
+            BCM2837_GPEDS0,
+            BCM2837_GPEDS1,
+            BCM2837_GPEDS_RESERVED,
+            BCM2837_GPREN0,
+            BCM2837_GPREN1,
+            BCM2837_GPREN_RESERVED,
+            BCM2837_GPFEN0,
+            BCM2837_GPFEN1,
+            BCM2837_GPFEN_RESERVED,
+            BCM2837_GPHEN0,
+            BCM2837_GPHEN1,
+            BCM2837_GPHEN_RESERVED,
+            BCM2837_GPLEN0,
+            BCM2837_GPLEN1,
+            BCM2837_GPLEN_RESERVED,
+            BCM2837_GPAREN0,
+            BCM2837_GPAREN1,
+            BCM2837_GPAREN_RESERVED,
+            BCM2837_GPAFEN0,
+            BCM2837_GPAFEN1,
+            BCM2837_GPAFEN_RESERVED,
+            BCM2837_GPPUD,
+            BCM2837_GPPUDCLK0,
+            BCM2837_GPPUDCLK1,
+            BCM2837_MAX
+        };
+
         using device_register = std::uint32_t volatile;
         using gpio_number = std::uint32_t;
 
@@ -15,7 +63,9 @@ class GPIO {
         }
 
         GPIO() {
-            std::printf("\nAddress of this 0x%X\n", this);
+            for(auto idx = 0; idx < Register::BCM2837_MAX; ++idx) {
+                std::printf("\nAddress of this 0x%X: ", &m_register[idx]);
+            }
         }
         ~GPIO() = default;
 
@@ -50,59 +100,8 @@ class GPIO {
         void set(gpio_number gpio);
         void clear(gpio_number gpio);
 
-        enum class Register: std::uint32_t {
-            /* GPIO Function Selection offset */
-            BCM2837_GPFSEL0	= 0x00000000,
-            BCM2837_GPFSEL1	= 0x00000001,
-            BCM2837_GPFSEL2	= 0x00000002,
-            BCM2837_GPFSEL3	= 0x00000003,
-            BCM2837_GPFSEL4	= 0x00000004,
-            BCM2837_GPFSEL5	= 0x00000005,
-            BCM2837_GPFSEL_RESERVED = 0x00000006,
-            /* GPIO SET Address offset */
-            BCM2837_GPSET0 = 0x00000007,
-            BCM2837_GPSET1 = 0x00000008,
-            BCM2837_GPSET_RESERVED = 0x00000009,
-            /* Offset for clearing the GPIO */
-            BCM2837_GPCLR0 = 0x0000000A,
-            BCM2837_GPCLR1 = 0x0000000B,
-            BCM2837_GPCLR_RESERVED = 0x0000000C,
-            /* Offset for GPIO LEV0/1 */
-            BCM2837_GPLEV0 = 0x0000000D,
-            BCM2837_GPLEV1 = 0x0000000E,
-            BCM2837_GPLEV_RESERVED = 0x0000000F,
-            BCM2837_GPEDS0 = 0x00000010,
-            BCM2837_GPEDS1 = 0x00000020,
-            BCM2837_GPEDS_RESERVED = 0x00000030,
-            BCM2837_GPREN0 = 0x00000040,
-            BCM2837_GPREN1 = 0x00000050,
-            BCM2837_GPREN_RESERVED = 0x00000060,
-            BCM2837_GPFEN0 = 0x00000070,
-            BCM2837_GPFEN1 = 0x00000080,
-            BCM2837_GPFEN_RESERVED = 0x00000090,
-            BCM2837_GPHEN0 = 0x000000A0,
-            BCM2837_GPHEN1 = 0x000000B0,
-            BCM2837_GPHEN_RESERVED = 0x000000C0,
-            BCM2837_GPLEN0 = 0x000000D0,
-            BCM2837_GPLEN1 = 0x000000E0,
-            BCM2837_GPLEN_RESERVED = 0x000000F0,
-            BCM2837_GPAREN0 = 0x00000100,
-            BCM2837_GPAREN1 = 0x00000200,
-            BCM2837_GPAREN_RESERVED = 0x00000300,
-            BCM2837_GPAFEN0 = 0x00000400,
-            BCM2837_GPAFEN1 = 0x00000500,
-            BCM2837_GPAFEN_RESERVED = 0x00000600,
-            BCM2837_GPPUD = 0x00000700,
-            BCM2837_GPPUDCLK0 = 0x00000800,
-            BCM2837_GPPUDCLK1 = 0x00000900,
-            /* 32bits Registers */
-            BCM2837_GPFSELn_INPUT = 0x00000000,
-            BCM2837_GPFSELn_OUTPUT = 0x00000001
-        };
-
-        
     private:
-        device_register m_register;
+        device_register m_register[Register::BCM2837_MAX];
 
 };
 
--- advanced/src/gpio.cpp
+++ advanced/src/gpio.cpp
@@ -8,49 +8,47 @@
 
 std::uint32_t GPIO::get(gpio_number gpio_n) {
     if(gpio_n < 32) {
-        return (*(m_register + Register::BCM2837_GPLEV0) &= (1U << gpio_n));
+        return (m_register[Register::BCM2837_GPLEV0] &= (1U << gpio_n));
 	}
 
-	return (*(m_register + Register::BCM2837_GPLEV1) &= (1U << (gpio_n - 32)));
+	return (m_register[Register::BCM2837_GPLEV1] &= (1U << (gpio_n - 32)));
 }
 
 void GPIO::out(gpio_number gpio_n) {
     ///@brief setting GPIO to output mode.
     /* sets the respective bit */					
-    *(m_register + (gpio_n / 10)) = (1U << (gpio_n % 10) * 3U);
+    m_register[(gpio_n / 10)] = (1U << (gpio_n % 10) * 3U);
 }
 
 void GPIO::in(gpio_number gpio_n) {
     ///@brief setting GPIO to input mode.
     /* bits positions keep changing based on GPIO Number */				
     /* sets the respective bit */					
-    *(&m_register + (gpio_n / 10)) = (~(7U << (gpio_n % 10U) * 3U));
+    m_register[(gpio_n / 10)] = (~(7U << (gpio_n % 10U) * 3U));
 }
 
 void GPIO::clear(gpio_number gpio_n) {
     if(gpio_n < 32) {
         /* sets the respective bit */					
-        *(m_register + Register::BCM2837_GPCLR0) = 1 << gpio_n;
+        m_register[Register::BCM2837_GPCLR0] = (1U << gpio_n);
 	} else {
         /*set the bits 0 - 21 for GPIO greater than 31 */					
-        *(m_register + Register::BCM2837_GPCLR1) = 1U << (gpio_n - 32);
+        m_register[Register::BCM2837_GPCLR1] = (1U << (gpio_n - 32));
 	}
 }
 
 void GPIO::set(gpio_number gpio_n) {
   
     if(gpio_n < 32) {
-        *(&m_register + Register::BCM2837_GPSET0) = (1U << gpio_n);
+        m_register[Register::BCM2837_GPSET0] = (1U << gpio_n);
     } else {
-        *(&m_register + Register::BCM2837_GPSET1) = (1U << (gpio_n - 32));
+        m_register[Register::BCM2837_GPSET1] = (1U << (gpio_n - 32));
 	}
 }
 
 
 std::int32_t main() {
-    GPIO *gpio = new GPIO;
-
-    gpio->in(12);
+    GPIO &gpio = *new GPIO;
 }
 
 
